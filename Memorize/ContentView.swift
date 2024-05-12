b88305f9b0b5523af20de2746e5656beaeb9421b
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2024-05-12 17:05:21 +0100

    End Lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,15 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    let contents = ["👻", "🕷️", "🦧", "🎃"]
+    let contents = ["👻", "🕷️", "🐵", "🎃", "👽", "👹", "🐉", "⛄️", "🐔", "☃️", "🧙‍♀️", "🥷", "👰🏻‍♂️", "🧝🏾‍♀️", "🧙🏼‍♂️" ]
+    @State var cardCount = 4
     
     var body: some View {
+        VStack{
+            ScrollView{
+                cards
+            }
+            Spacer()
+            cardCountAdjuster
+        }
+        .padding()
+    }
+    
+    var cardCountAdjuster: some View {
         HStack{
-            ForEach(contents.indices, id: \.self) { index in
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    var cards: some View {
+        LazyVGrid (columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: contents[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
-        .padding().foregroundColor(.orange)
+        .foregroundColor(.orange)
+    }
+    
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > contents.count)
+    }
+    
+    var cardRemover: some View {
+        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+    }
+    
+    var cardAdder: some View {
+        return cardCountAdjuster(by: +1, symbol: "rectangle.stack.fill.badge.plus")
     }
 }
 
@@ -27,29 +68,19 @@ struct CardView: View {
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/, style: .continuous)
-            if isFaceUp{
+            Group {
                 base.strokeBorder(lineWidth: 2).foregroundColor(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                base.fill()
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {
             isFaceUp.toggle()
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
 
 #Preview {
     ContentView()
